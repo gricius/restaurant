1741b4eb4f8d6f327ca0394e80e49bf29f57d26d
Andy <92579239+gricius@users.noreply.github.com>
2023-07-15 14:24:54 +0000

    add webpack loader for styles and images

--- README.md
+++
@@ -1,3 +0,0 @@
-TheOdinProject JS Project: Restaurant Page
-Assignment details: https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page
-Live preview: https://gricius.github.io/restaurant/
--- src/pageLoad.js
+++
@@ -1,9 +0,0 @@
-import createRestaurantHomePage from './restaurant';
-import createTabs from './tabs';
-
-function initialLoad() {
-    createTabs();
-    createRestaurantHomePage();
-}
-
-export default initialLoad; 
